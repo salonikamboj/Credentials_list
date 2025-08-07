8113d6d203923b16ba2916752089f048ff178c11
saloni <skamboj8103@gmail.com>
2025-08-07 15:06:48 -0400

    add intro screen

---
+++ app/src/main/java/com/example/sen800_project/IntroActivity.java
@@ -0,0 +1,25 @@
+package com.example.sen800_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        Button continueBtn = findViewById(R.id.continue_button);
+        continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // So user can't go back to intro screen
+            }
+        });
+    }
+}
